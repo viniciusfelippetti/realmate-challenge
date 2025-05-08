1c95b877ca5cacaade871bf76f0590992c84750a
viniciusfelippetti <felippettivinicius@gmail.com>
2025-05-08 18:19:42 -0300

    ajustes nas urls

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('conversations/', conversation_list_view, name='conversation_list'),
     path('conversations/<uuid:id>/', conversation_detail_view, name='conversation_detail'),
     path('webhook/', WebhookCreateView.as_view(), name='webhook'),
-    path('conversations/<uuid:id>/', ConversationDetailView.as_view(), name='conversation_detail'),
+    path('conversation/<uuid:id>/', ConversationDetailView.as_view(), name='api-conversation_detail'),
 ]
